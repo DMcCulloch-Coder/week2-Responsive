b9793722a168f37d7b1845463b164819efeaaff6
David <DMcCulloch86@gmail.com>
2019-09-20 17:19:24 -0400

    added files from portfolio, made header and body responsive to smaller windows

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById('submitButton').onclick = function (){
+   
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    alert('ERROR! Please use e-mail link to contact user.')
+
+    //console.log(name + email + message)
+
+    //find a way to export this data
+
+}
+
